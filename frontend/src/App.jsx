065e53c46f3f8038eee9e8dbd1af41eafddb94bb
Gabriel Tamarossi <gabrieltamarossi@gmail.com>
2026-04-02 21:48:48 -0300

    fix:routes bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home/home";
-import About from "./pages/about/about";
-import CreateCampaign from "./pages/createCampaign";
-import Graphics from "./pages/graphics";
-import Settings from "./pages/settings";
-import Templates from "./pages/templates";
-import Users from "./pages/users";
+import AppRoutes from "./routes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<CreateCampaign />} />
-        <Route path="/graphics" element={<Graphics />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/templates" element={<Templates />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-import Home from '../pages/home/home'
-import Login from '../pages/login/login'
-import AdminPortal from '../pages/admin/admin'
+import Login from "../pages/login/login"
+import Home from "../pages/home/home"
+import AdminPortal from "../pages/admin/admin"
+import About from "../pages/about/about"
+import CreateCampaign from "../pages/createCampaign"
+import Graphics from "../pages/graphics"
+import Settings from "../pages/settings"
+import Templates from "../pages/templates"
+import Users from "../pages/users"
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/admin" element={<AdminPortal />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<CreateCampaign />} />
+        <Route path="/graphics" element={<Graphics />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/templates" element={<Templates />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
