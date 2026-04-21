7f7d0dc858bfddae844a0b682b8c472423bb3263
Caio Franson <caio.franson@gmail.com>
2026-04-21 18:46:34 -0300

    Fixed background issue

--- frontend/src/layouts/AppShellLayout.jsx
+++ frontend/src/layouts/AppShellLayout.jsx
@@ -6,14 +6,14 @@ import AppNavbar from "@/components/navigation/AppNavbar";
 export default function AppShellLayout() {
     return (
         <div className="relative isolate min-h-screen overflow-hidden px-3 py-4 sm:px-5">
-            <div aria-hidden="true" className="pointer-events-none absolute inset-0 z-0">
+            <div aria-hidden="true" className="pointer-events-none fixed inset-0 z-0 bg-[#051524]">
                 <AnimatedBackground />
             </div>
 
             <div
                 aria-hidden="true"
                 className="pointer-events-none absolute inset-x-0 top-0 z-10 
-                mx-auto h-[400px] w-[min(1230px,98vw)] rounded-[48px] 
+                mx-auto h-100 w-[min(1230px,98vw)] rounded-[48px] 
                 bg-linear-to-t from-teal-600 to-cyan-900 opacity-30"
             />
 
