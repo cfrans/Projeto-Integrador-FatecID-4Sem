9d385eda3e266fec0ef07163bf00e73b167af132
Caio Franson <caio.franson@gmail.com>
2026-04-26 13:07:52 -0300

    alterado peixe-icons de .svg para .png para melhor performance

--- frontend/src/components/ui/modal.jsx
+++ frontend/src/components/ui/modal.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
-import peixeSurpreso from '@/assets/peixe-icons/peixe-icon-surpreso.svg'
-import peixeHappy from '@/assets/peixe-icons/peixe-icon-happy.svg'
-import peixeDuvidoso from '@/assets/peixe-icons/peixe-icon-duvidoso.svg'
+import peixeSurpreso from '@/assets/peixe-icons/peixe-icon-surpreso.png'
+import peixeHappy from '@/assets/peixe-icons/peixe-icon-happy.png'
+import peixeDuvidoso from '@/assets/peixe-icons/peixe-icon-duvidoso.png'
 
 export default function Modal({
   open,
